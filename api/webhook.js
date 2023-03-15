9d2fda5db952b13e986cf564b4cb3b1225b55b17
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2023-03-15 10:39:33 +0900

    Vercel /webhook complete

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,49 @@
 export default async function handler(req, res) {
-  const { body } = req;
-  return res.send(`This is webhook endpoint! ${body.name}`);
+  if (req.body.events[0].type === "message") {
+    // Message data, must be stringified
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          "type": "text",
+          "text": "Hello, user"
+        },
+        {
+          "type": "text",
+          "text": "May I help you?"
+        }
+      ]
+    })
+
+    // Request header
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + TOKEN
+    }
+
+    // Options to pass into the request
+    const webhookOptions = {
+      "hostname": "api.line.me",
+      "path": "/v2/bot/message/reply",
+      "method": "POST",
+      "headers": headers,
+      "body": dataString
+    }
+
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    // Handle error
+    request.on("error", (err) => {
+      console.error(err)
+    })
+
+    // Send data
+    request.write(dataString)
+    request.end()
+  }
 }
