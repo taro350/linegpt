a9e4d41808640f86dd28785cf98a1b23cedc59aa
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2023-03-17 15:52:43 +0900

    move verbose error message /api

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
 
     return res.send(`Here's the answer : ${a}`);
   } else {
-    res.status(400).send('400 Error [INVALID_PAYLOAD] Your request does not include JSON data!')
+    res.status(400).send("400 Error [INVALID_PAYLOAD] Your request does not include JSON data `question' parameter for GPT!")
   }
 
   }
