10fd9f8f2333ff0bf6dd683ca3f617bc6d64bf29
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2023-03-17 17:01:13 +0900

    copy gpt request code to /webhook

--- api/index.js
+++ api/index.js
@@ -9,34 +9,44 @@ const openai = new OpenAIApi(configuration);
 
 async function askGpt3DominosQuestion(question) {
   const prompt = `
-    The following is a conversation with an AI specifically trained on Domino's Pizza information. It can provide helpful answers to any questions related to Domino's Pizza, including products, services, promotions, and more.
+    The following is a conversation with an AI specifically trained on Domino Pizza information. It can provide helpful answers to any questions related to Domino's Pizza, including products, services, promotions, and more.
 
     User: ${question}
     AI:`;
 
   const completion = await openai.createCompletion({
-    engine: "davinci-codex",
+    model: "davinci-codex",
     prompt: prompt,
     max_tokens: 100,
     n: 1,
     stop: null,
     temperature: 0.75,
   });
-  const answer = completion.data.choices[0].text
-  console.log(answer)
-
+  const answer = completion.data
+  console.log("******************************************************************")
+  console.log("Here's the answer form GPT model: " +  answer)
+  console.log("******************************************************************")
   return answer;
 }
 
 export default async function handler(req, res) {
+  console.log("You are at /api endpoint")
   if (req.body) {
-    const { body } = req;
-    
     try {
-      const ans = askGpt3DominosQuestion(body.question)
+      console.log(`Question : "${req.body.question}"`)
+      const ans = await askGpt3DominosQuestion(req.body.question)
+      console.log(ans)  // <pending>
+      console.log(`Promise resolved!: ${ans}`)
+
       return res.send({"answer" : ans});
-    } catch (e) {
-      console.log(`--- Error occured! : ${e}`)
+    } catch (error) {
+      console.log(`--- Error occured! : *****${error}*****`)
+      if (error.response) {
+        console.log("--- Error message : " + error.response.status);
+        console.log("--- Error message : " + error.response.data);
+      } else {
+        console.log(`--- Error message : ${error.message}`);
+      }
     }
   } else {
     res.status(400).send("400 Error [INVALID_PAYLOAD] Your request does not include JSON data 'question' parameter for GPT!")
--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,5 @@
+import { Configuration, OpenAIApi } from "openai";
+
 var https;
 
 (async() => {
@@ -15,60 +17,101 @@ var https;
 const TOKEN = process.env.LINE_ACCESS_TOKEN
 
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function askGpt3DominosQuestion(question) {
+  const prompt = `
+    The following is a conversation with an AI specifically trained on Domino Pizza information. It can provide helpful answers to any questions related to Domino's Pizza, including products, services, promotions, and more.
+
+    User: ${question}
+    AI:`;
+
+  const completion = await openai.createCompletion({
+    model: "davinci-codex",
+    prompt: prompt,
+    max_tokens: 100,
+    n: 1,
+    stop: null,
+    temperature: 0.75,
+  });
+  const answer = completion.data
+  console.log("******************************************************************")
+  console.log("Here's the answer form GPT model: " +  answer)
+  console.log("******************************************************************")
+  retur
+
 export default async function handler(req, res) {
   if (req.body) {
     console.log("---- You send us JSON data at /webhook endpoint");
   
     if (req.body.events[0].type === "message") {
-      
-      // Request to OpenAI GPT4 here
-      const msg = [
+      const m = req.body.events[0].message.text
+      try {
+        console.log("Question : " + m)
+        const answer = await askGpt3DominosQuestion(m)
+        console.log(answer)  // <pending>
+        console.log(`Promise resolved!: ${answer}`)
+
+        // return res.send({"answer" : ans});
+
+        const msg = [
           {
             "type": "text",
-            "text": "Hello, user"
-          },
-          {
-            "type": "text",
-            "text": "May I help you?"
+            "text": answer
           }
         ]
 
-      // Message data, must be stringified
-      const dataString = JSON.stringify({
-        replyToken: req.body.events[0].replyToken,
-        messages: msg
-      })
-
-      // Request header
-      const headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + TOKEN
-      }
-
-      // Options to pass into the request
-      const webhookOptions = {
-        "hostname": "api.line.me",
-        "path": "/v2/bot/message/reply",
-        "method": "POST",
-        "headers": headers,
-        "body": dataString
-      }
-
-      // Define request
-      const request = await https.request(webhookOptions, (res) => {
-        res.on("data", (d) => {
-          process.stdout.write(d)
+        // Message data, must be stringified
+        const dataString = JSON.stringify({
+          replyToken: req.body.events[0].replyToken,
+          messages: msg
         })
-      })
 
-      // Handle error
-      request.on("error", (err) => {
-        console.error(err)
-      })
+        // Request header
+        const headers = {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + TOKEN
+        }
 
-      // Send data
-      request.write(dataString)
-      request.end()
+        // Options to pass into the request
+        const webhookOptions = {
+          "hostname": "api.line.me",
+          "path": "/v2/bot/message/reply",
+          "method": "POST",
+          "headers": headers,
+          "body": dataString
+        }
+
+        // Define request
+        const request = await https.request(webhookOptions, (res) => {
+          res.on("data", (d) => {
+            process.stdout.write(d)
+          })
+        })
+
+        // Handle error
+        request.on("error", (err) => {
+          console.error(err)
+        })
+
+        // Send data
+        request.write(dataString)
+        request.end()
+
+      } catch (error) {
+        console.log(`--- Error occured! : *****${error}*****`)
+        if (error.response) {
+          console.log("--- Error message : " + error.response.status);
+          console.log("--- Error message : " + error.response.data);
+        } else {
+          console.log(`--- Error message : ${error.message}`);
+        }
+      }
+      
     } else {
       res.status(400).send('400 Error [INVALID_PAYLOAD]: events[0].type is not message')
     }
